8e2197af20fa217cbed6991dba904a105ceed3c6
johnnymatthews <9611008+johnnymatthews@users.noreply.github.com>
2022-12-29 15:53:08 -0500

    Removes DarkMode content from font-switcher.

--- assets/js/open-dyslexic.js
+++ assets/js/open-dyslexic.js
@@ -2,16 +2,14 @@ const mode = document.getElementById('font-switcher');
 
 if (mode !== null) {
 
-  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+  window.addEventListener('change', event => {
 
     if (event.matches) {
 
-      localStorage.setItem('theme', 'dark');
       document.documentElement.setAttribute('data-dyslexic-font', '');
 
     } else {
 
-      localStorage.setItem('theme', 'light');
       document.documentElement.removeAttribute('data-dyslexic-font');
 
     }
@@ -21,7 +19,6 @@ if (mode !== null) {
   mode.addEventListener('click', () => {
 
     document.documentElement.toggleAttribute('data-dyslexic-font');
-    localStorage.setItem('theme', document.documentElement.hasAttribute('data-dyslexic-font') ? 'dark' : 'light');
 
   });
 
